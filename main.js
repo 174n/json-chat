ead154ae380bc99298055a2b334cd0c0270e38fc
Ivan Alexandrov <ivan@alexandrov.online>
2021-04-08 00:45:47 +0200

    Add timestamps

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import { EvpKDF } from "crypto-es/lib/evpkdf";
 import { Utf8 } from "crypto-es/lib/core";
 import { escape } from "html-sloppy-escaper";
 import { observe } from "fast-json-patch";
+import { identicon } from "minidenticons";
 
 document.body.setAttribute("style", "");
 
@@ -68,47 +69,23 @@ window.browserFingerprint = getHash([
     }).join(';')
   ].join('###')).toString();
 
-// Identicon
-const e="d4145a 8e78ff ff7300 fbb03b ed1e79 019244 ed1c23 2e3192 fc7d7b fecc00 3aa17e 4f00bc 09c9be 662d8c 00a8c5 1353ae".split(" ");window.identicon=function(t){const n=t.split("").map((e=>e.charCodeAt(0))).reduce(((e,t)=>16777619*((e^t)>>>0)),2166136261);return`<svg viewBox="-1.5 -1.5 8 8" xmlns="http://www.w3.org/2000/svg" fill="#${e[n/16777619%e.length]||""}">${t?[...Array(25).keys()].map((e=>n%(16-e%15)<4?`<rect x="${e>14?7-~~(e/5):~~(e/5)}" y="${e%5}" width="1" height="1"/>`:"")).join(""):[]}</svg>`};const identiconSvg=globalThis.customElements?.define("identicon-svg",class extends HTMLElement{constructor(){super()}connectedCallback(){this.identiconSvg()}attributeChangedCallback(){this.identiconSvg()}static get observedAttributes(){return["username"]}identiconSvg(){this.innerHTML=identicon(this.getAttribute("username"))}});
-
 // https://github.com/TinyLibraries/tiny-mark
-const tinymark = str => {
-  return str
-    .replace(/(_[^*_]+)\*([^*_]+_)/g, "$1_*_$2") // Remove interlacing
-    .replace(/\*([^*]+)\*/g, "<b>$1</b>") // Bold
-    .replace(/_([^_]+)_/g, "<em>$1</em>") // Italic
-    .replace(/!\[([^\]]{0,255})\]\((https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*))\)/g, '<img src="$2" alt="$1">') // Image
-    .replace(/\n/g, "<br />"); // New Line
-};
+const urlRegex = '(https?:\\/\\/(www\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*))';
+const tinymark = str => str
+  .replace(/(_[^*_]+)\*([^*_]+_)/g, "$1_*_$2") // Remove interlacing
+  .replace(/\*([^*]+)\*/g, "<b>$1</b>") // Bold
+  .replace(/_([^_]+)_/g, "<em>$1</em>") // Italic
+  .replace(new RegExp(`(?<!\\]\\()${urlRegex}`, "g"), '<a href="$1" target="_blank">$1</a>') // Link
+  .replace(new RegExp(`!\\[([^\\]]{0,255})\\]\\(${urlRegex}\\)`, "g"), '<img src="$2" alt="$1">') // Image
+  .replace(/\n/g, "<br />"); // New Line
+
+// tinyAgo
+const ago=e=>{let t,n,r=0|(Date.now()-e)/1e3,o={"сек":60,"мин":60,"ч":24};for(t in o)if(n=r%o[t],!(r=0|r/o[t]))return n+" "+t+". назад";return new Date(e).toLocaleString("ru")};
+const updateTimestamps = (start=0) => [...document.querySelectorAll("[data-date]")].slice(start).forEach(d => d.innerText = ago(parseInt(d.getAttribute("data-date"))));
+setInterval(() => updateTimestamps(-3), 1000);
+setInterval(updateTimestamps, 60000);
 
 window.navestiSuetu=()=>setInterval(()=>{document.querySelector(".icon-bg").innerHTML=Array.from({length:(window.innerHeight+window.innerWidth)/10}).map(()=>identicon(Math.random().toString())).join("\n")},500);
-
-// const checkIfUsed = () => {
-//   window.addEventListener("click", () => {
-//     window.chatUsed = true;
-//   });
-//   window.addEventListener("keyup", () => {
-//     window.chatUsed = true;
-//   });
-//   window.addEventListener("scroll", () => {
-//     window.chatUsed = true;
-//   });
-
-//   setInterval(() => {
-//     if (window.chatUsed) {
-//       window.chatUsed = false;
-//     } else if (msgInputEl.value === "") {
-//       location.reload();
-//     }
-//   }, 2 * 60 * 1000);
-// }
-
-const parseDate = timestamp => {
-  const date = new Date(timestamp);
-  const currentDate = new Date();
-  return currentDate.toLocaleString("ru").slice(0, 10) === date.toLocaleString("ru").slice(0, 10)
-    ? date.toLocaleTimeString("ru") : date.toLocaleString("ru");
-}
 
 const decryptMessage = msg =>
   JSON.parse(AES.decrypt(msg.data, window.chatPass).toString(Utf8));
@@ -146,7 +123,7 @@ const pushMessage = (encrypted, id) => {
       <div class="fingerprint">[${fingerprint.slice(0, 10)}]</div>
     </div>
     <div class="text">${tinymark(escape(msg))}</div>
-    <div class="date">${parseDate(date)}</div>
+    <div class="date" data-date="${date}"></div>
   `;
   message.append(avatar);
   message.append(msgInner);
@@ -204,6 +181,7 @@ window.setLongpollMessages = msgs => {
   msgsEl.classList.remove("loading");
   msgsEl.scrollTop = msgsEl.scrollHeight;
   scrollWhenImagesLoaded();
+  updateTimestamps();
   observeMessages();
 }
 
@@ -252,6 +230,7 @@ const loadChat = async (firstTime) => {
       if (firstTime) {
         observeMessages();
       }
+      updateTimestamps();
     }, 300);
     if (!chatLongpollSupported) {
       setTimeout(loadChat, 15000)
@@ -278,7 +257,6 @@ passwordFormEl.addEventListener("submit", e => {
   chatNameEl.value = "";
 
   loadChat(true);
-  // checkIfUsed();
 });
 
 msgFormEl.addEventListener("submit", async e => {
